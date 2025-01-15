853200f137c68ef33a12a1aabd3369944bc5cf75
Marcelo <marcelomgarcia@gmail.com>
2025-01-15 20:58:52 +0300

    Query as argument to execute

--- movieclubsched.py
+++ movieclubsched.py
@@ -4,15 +4,12 @@
 import csv
 from sqlite3 import connect
 
-
 def main():
     with connect("movie_club.db") as conn:
         cur = conn.cursor()
-        query = """
-        select movies.title, directors.name, movies.year, movies.country from movies 
-        INNER JOIN directors on movies.director_id=directors.id 
-        ORDER by screen_date;"""
-        cur.execute(query)
+        cur.execute("""select movies.title, directors.name, movies.year, 
+        movies.country from movies INNER JOIN directors on 
+        movies.director_id=directors.id ORDER by screen_date;""")
         rows = cur.fetchall()
         print(len(rows))
         for row in rows:
